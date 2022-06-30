8bf40f79b3625b6b37f5da10c2992a0f574f400e
Suleyman Sezer <me.iamcodder@gmail.com>
2022-07-01 00:57:13 +0300

    Hms login tried

--- app/src/main/java/com/patronusstudio/productflavorexample/MainActivity.kt
+++ app/src/main/java/com/patronusstudio/productflavorexample/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.patronusstudio.productflavorexample
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.huawei.hms.support.hwid.HuaweiIdAuthManager
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
+import com.huawei.hms.support.hwid.service.HuaweiIdAuthService
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<RelativeLayout>(R.id.btn_login).setOnClickListener {
+            val authParams: HuaweiIdAuthParams =
+                HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                    .setAuthorizationCode()
+                    .setProfile()
+                    .createParams()
+            val service: HuaweiIdAuthService = HuaweiIdAuthManager.getService(this, authParams)
+            startActivityForResult(service.signInIntent, 99)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === 99) {
+            //login success
+            val authHuaweiIdTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
+            if (authHuaweiIdTask.isSuccessful) {
+                val huaweiAccount = authHuaweiIdTask.result
+            } else {
+
+            }
+        }
     }
 }
