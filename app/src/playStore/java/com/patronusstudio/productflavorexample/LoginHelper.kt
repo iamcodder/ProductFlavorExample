176210a3125d279f05525bc49b03162b1ed7a75b
Suleyman Sezer <me.iamcodder@gmail.com>
2022-07-10 23:50:01 +0300

    Google login added

--- app/src/playStore/java/com/patronusstudio/productflavorexample/LoginHelper.kt
+++ app/src/playStore/java/com/patronusstudio/productflavorexample/LoginHelper.kt
@@ -2,14 +2,24 @@ package com.patronusstudio.productflavorexample
 
 import android.content.Context
 import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
 
 class LoginHelper(private val context: Context) : BaseLogin() {
 
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
     override fun init() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(context, gso)
     }
 
     override fun getLoginIntent(): Intent {
-        return Intent()
+        return mGoogleSignInClient.signInIntent
     }
 
 }
